9038a0c277735fb9e4e1950746c70491a288008b
Nathaniel Quashie <nathanielcfa@gmail.com>
2016-09-19 13:52:09 +0000

    Add scrape example 1

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var fs = require('fs');
 var app = express();
 var port = process.env.PORT;
 
+//Example 1
+// var url = "http://www.google.com";
+
+// request(url, function(error, response, body){
+//     if(error){
+//         console.log(error);
+//     } else {
+//         console.log(body);
+//     }
+// });
+
+//Example 2
+
+
 app.listen(port, function(){
     console.log("Alive on " + port)
 })
